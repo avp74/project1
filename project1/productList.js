ca863f8ce2c582c76c691d99d61b7e590776827b
avp74 <60273800+avp74@users.noreply.github.com>
2021-02-24 12:14:31 -0500

    basic html and css

---
+++ project1/productList.js
@@ -0,0 +1,15 @@
+// (function () {
+//
+//     var mockDatabase = [
+//         {id: 1, name: 'Chocolate Chip Cookies', price: "3.50", published: true},
+//         {id: 2, name: 'Macadamia Nut Cookies', price: "3.50", published: true},
+//         {id: 3, name: 'Peanut Butter Cookies', price: "3.50", published: false},
+//         {id: 4, name: 'Oatmeal Raisin Cookies', price: "3.50", published: false},
+//         {id: 5, name: 'M&M Cookies', price: "3.75", published: true},
+//         {id: 6, name: 'PB&J Cookies', price: "4.00", published: false},
+//         {id: 7, name: 'Smores Cookies', price: "4.50", published: true},
+//         {id: 8, name: 'Strawberry Shortcake Cookies', price: "4.75", published: false},
+//         {id: 9, name: 'Espresso Chocolate Chip Cookies', price: "5.00", published: true},
+//     ];
+//
+//
